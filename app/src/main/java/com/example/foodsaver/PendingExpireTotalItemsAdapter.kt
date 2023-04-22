c57bc5905d45433c397a4ee263502a1a63df6d18
NZpatelK <patelk.dev@hotmail.com>
2023-04-22 21:09:34 +1200

    add cardview animation

--- app/src/main/java/com/example/foodsaver/PendingExpireTotalItemsAdapter.kt
+++ app/src/main/java/com/example/foodsaver/PendingExpireTotalItemsAdapter.kt
@@ -3,13 +3,16 @@ package com.example.foodsaver
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
 class PendingExpireTotalItemsAdapter (private val pendingExpireTotalItemsList: ArrayList<PendingExpireTotalItems>) : RecyclerView.Adapter<PendingExpireTotalItemsAdapter.PendingExpireTotalItemsHolder> () {
     class PendingExpireTotalItemsHolder(view: View) : RecyclerView.ViewHolder(view) {
         val outputDate: TextView = view.findViewById(R.id.outputDate)
         val totalItems: TextView = view.findViewById((R.id.totalItems))
+        val cardView: CardView = view.findViewById(R.id.pendingExpireItemsCard)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PendingExpireTotalItemsHolder {
@@ -24,7 +27,10 @@ class PendingExpireTotalItemsAdapter (private val pendingExpireTotalItemsList: A
     override fun getItemCount(): Int = pendingExpireTotalItemsList.size
 
     override fun onBindViewHolder(holder: PendingExpireTotalItemsHolder, position: Int) {
-        holder.outputDate.text = pendingExpireTotalItemsList[position].date.toString()
+        holder.outputDate.text = pendingExpireTotalItemsList[position].date
         holder.totalItems.text = pendingExpireTotalItemsList[position].totalItems.toString()
+
+        holder.cardView.startAnimation(AnimationUtils.loadAnimation(holder.itemView.context, R.anim.scale_up))
+
     }
 }
