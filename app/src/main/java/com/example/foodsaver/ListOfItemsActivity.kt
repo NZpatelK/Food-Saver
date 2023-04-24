a9102712f9f8f8a2592c8adfa500aa7a01c976a1
NZpatelK <patelk.dev@hotmail.com>
2023-04-25 10:20:31 +1200

    add float button in list item page

--- app/src/main/java/com/example/foodsaver/ListOfItemsActivity.kt
+++ app/src/main/java/com/example/foodsaver/ListOfItemsActivity.kt
@@ -1,13 +1,17 @@
 package com.example.foodsaver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.widget.TextView
+import androidx.core.app.ActivityOptionsCompat
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.color.MaterialColors
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.transition.platform.MaterialArcMotion
 import com.google.android.material.transition.platform.MaterialContainerTransform
 import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback
@@ -27,6 +31,14 @@ class ListOfItemsActivity : AppCompatActivity() {
 
         val ListOfProducts = intent.getSerializableExtra("productsList") as List<Item>
 
+        val fab = findViewById<ExtendedFloatingActionButton>(R.id.floating_action)
+
+        fab.setOnClickListener {
+            val intent = Intent(this, AddItemActivity::class.java)
+            val option = ActivityOptionsCompat.makeSceneTransitionAnimation(this,  fab,  fab.transitionName )
+            startActivity(intent, option.toBundle())
+        }
+
         if (ListOfProducts != null) {
             val prodRecyclerView = findViewById<RecyclerView>(R.id.itemRecyclerView)
             prodRecyclerView.apply {
@@ -34,10 +46,47 @@ class ListOfItemsActivity : AppCompatActivity() {
                 setHasFixedSize(true)
                 adapter = ItemAdapter(ListOfProducts)
             }
+
+            scrollListener(prodRecyclerView, fab)
+
         } else {
             // Handle the case when name is null or not of type List<Items>
         }
 
+
+    }
+
+    private fun scrollListener(prodRecyclerView: RecyclerView, fab: ExtendedFloatingActionButton) {
+
+        prodRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                // Scroll down
+                if (dy > 20 && fab.isExtended) {
+                    fab.shrink()
+                    fab.visibility = View.VISIBLE
+                }
+
+                // Scroll up
+                if (dy < -20 && !fab.isExtended) {
+                    fab.extend()
+                    fab.visibility = View.VISIBLE
+                }
+
+                // At the top
+                if (!recyclerView.canScrollVertically(-1)) {
+                    fab.extend()
+                    fab.visibility = View.VISIBLE
+                }
+
+                // At the top
+                if (!recyclerView.canScrollVertically(1)) {
+                    fab.visibility = View.GONE
+                }
+            }
+        })
 
     }
 
