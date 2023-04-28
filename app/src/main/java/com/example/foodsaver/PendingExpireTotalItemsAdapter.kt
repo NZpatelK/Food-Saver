1483db9541e71c9253bbd4b62e809431e8530ac7
NZpatelK <patelk.dev@hotmail.com>
2023-04-29 11:26:49 +1200

    Update PendingExpireTotalItemsAdapter.kt

--- app/src/main/java/com/example/foodsaver/PendingExpireTotalItemsAdapter.kt
+++ app/src/main/java/com/example/foodsaver/PendingExpireTotalItemsAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.foodsaver
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.view.LayoutInflater
@@ -7,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.TextView
-import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +15,9 @@ import java.io.Serializable
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+/**
+ * Adapter Class for Controlling View of List of Pending Expiry Dates with Total Items Expiring on the Same Date Page
+ */
 class PendingExpireTotalItemsAdapter(private var pendingExpireTotalItemsList: Map<LocalDate, List<Item>>) : RecyclerView.Adapter<PendingExpireTotalItemsAdapter.PendingExpireTotalItemsHolder> () {
     class PendingExpireTotalItemsHolder(view: View) : RecyclerView.ViewHolder(view) {
         val outputDate: TextView = view.findViewById(R.id.outputDate)
@@ -31,6 +34,7 @@ class PendingExpireTotalItemsAdapter(private var pendingExpireTotalItemsList: Ma
 
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setPendingExpireTotalItems(newList: Map<LocalDate, List<Item>>) {
         pendingExpireTotalItemsList = newList
         notifyDataSetChanged()
@@ -46,6 +50,8 @@ class PendingExpireTotalItemsAdapter(private var pendingExpireTotalItemsList: Ma
         holder.outputDate.text = key.format(formatter)
         holder.totalItems.text = pendingExpireTotalItemsList[key]?.size.toString()
 
+        //This is show colour of the card to alert the user that items will expire very soon.
+        //This code is temporary, and it will change with the actual date. Right now, it uses an array index for demonstration purposes only.
         when (position) {
             0 -> {
                 holder.cardView.setBackgroundResource(R.drawable.alert_rounded_cardview)
@@ -60,6 +66,7 @@ class PendingExpireTotalItemsAdapter(private var pendingExpireTotalItemsList: Ma
 
         holder.cardView.startAnimation(AnimationUtils.loadAnimation(holder.itemView.context, R.anim.scale_up))
 
+        // This is click listener when user click on the specific expire date to open a new page show the list of items that expire on the same date.
         holder.cardView.setOnClickListener {
 
             val intent = Intent(holder.itemView.context, ListOfItemsActivity::class.java)
